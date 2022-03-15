8526a82f1ce2fb8e7ad53f920bbbf1c81a5fab1b
AlexsandroD <adrdra88@gmail.com>
2022-03-15 11:11:33 +0100

    front end init

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Main /> 
+  </div>
+</template>
+
+<script>
+import Main from "./components/macro/Main.vue";
+export default {
+  name: "App",
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
