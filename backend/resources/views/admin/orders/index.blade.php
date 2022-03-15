cf633a54f75c751fb16e635b6fd7f4c222f367bd
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2022-03-15 15:53:18 +0100

    swich_language_to_ita

--- backend/resources/views/admin/orders/index.blade.php
+++ backend/resources/views/admin/orders/index.blade.php
@@ -2,22 +2,22 @@
 
 
 @section('content')
-    <h1>Orders</h1>
+    <h1>Ordini</h1>
     @foreach($orders as $order)
-    <p>Name: {{ $order->customer_name}} Surname: {{ $order->customer_surname }}</p>
+    <p>Nome: {{ $order->customer_name}} Cognome: {{ $order->customer_surname }}</p>
     <p>Email: {{ $order->customer_email}}</p>
-    <p>Address: {{ $order->customer_address }}, {{ $order->customer_city }}, {{ $order->customer_post_code }}, {{ $order->customer_country  }}</p>
-    <p>Phone number: {{ $order->customer_phone }}</p>
-    <p>Comment: {{ $order->customer_comment }}</p>
-    <p>Total: {{ $order->tot_price }}</p>
+    <p>Indirizzo: {{ $order->customer_address }}, {{ $order->customer_city }}, {{ $order->customer_post_code }}, {{ $order->customer_country  }}</p>
+    <p>Numero di telefono: {{ $order->customer_phone }}</p>
+    <p>Commento: {{ $order->customer_comment }}</p>
+    <p>Totale: {{ $order->tot_price }}</p>
 
         @foreach($order->dishes as $dish)
-            <p>Dish's name: {{ $dish->name }}</p>
-            <p>Quantity: {{ $dish->pivot->quantity }}</p>
+            <p>Nome del piatto: {{ $dish->name }}</p>
+            <p>Quantità: {{ $dish->pivot->quantity }}</p>
         @endforeach
 
     @if($order->accepted)
-        <span class="badge badge-success">Accepted</span>
+        <span class="badge badge-success">Accettato</span>
     @else
         <a href="{{ route('orders.update', $order->id) }}"><button type="button" class="btn btn-info">Accetta</button></a>
         {{-- modal delete button --}}
@@ -32,7 +32,7 @@
                         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span></button>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
                         {{-- delete form --}}
                         <form action="{{route('orders.destroy', $order->id)}}" method="POST">
                             @csrf
