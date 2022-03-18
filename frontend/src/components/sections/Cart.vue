0ddb47769b8e3986563e17d8805fd81ce038729f
AlexsandroD <adrdra88@gmail.com>
2022-03-18 09:46:47 +0100

    last fix

--- frontend/src/components/sections/Cart.vue
+++ frontend/src/components/sections/Cart.vue
@@ -4,10 +4,16 @@
       <p v-if="cartLogic.restaurantId == cartLogic.newRestaurantId">{{cartLogic.restaurantName}}</p>
       <ul v-if="cartLogic.restaurantId == cartLogic.newRestaurantId">
           <li v-for='dish in cartLogic.cart' :key='dish.dishId'>
-              {{ dish.name }}
+              <div>
+                {{ dish.name }} 
+                <button @click="cartLogic.removeCartItem(dish.dishId)">-</button>
+                <span>{{ dish.quantity }}</span>
+                <button @click="cartLogic.addCartItem(dish.dishId,dish.name,dish.price,cartLogic.restaurantId,cartLogic.restaurantSlug,cartLogic.restaurantName)">+</button>
+              </div>
               {{ returnDishTotal(dish.dishPrice,dish.quantity) }}
           </li>
       </ul>
+      <button v-if="cartLogic.restaurantId == cartLogic.newRestaurantId || cartLogic.cart.length > 0" @click="cartLogic.emptyCart()">Svuota carrello</button>
       <p v-if="cartLogic.restaurantId == cartLogic.newRestaurantId">totale:{{cartLogic.totalPrice}}</p>
       <p v-else>carrello vuoto</p>
   </div>
