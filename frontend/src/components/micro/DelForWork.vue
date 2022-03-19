05a92388b32ead080602e66c5c901e8c66e9db0f
AlexsandroD <adrdra88@gmail.com>
2022-03-19 17:34:37 +0100

    deliveroo for work comonpent

---
+++ frontend/src/components/micro/DelForWork.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container">
+      <div class="image"></div>
+      <div class="text">
+          <h2>Deliveroo for work</h2>
+          <p>CLienti o colleghi affamati? il nostro team Corporate ti puó aiutare.</p>
+          <a class="btn" href="">Contattaci</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+    @media screen and (min-width: 100px) {
+     .container{
+        display:grid;
+        background-color: rgb(249, 250, 250);
+        width: 100%;
+        min-height: 200px;
+        margin: auto;
+        grid: auto-flow / 1fr 1fr;
+        .image{
+            background-image: url('../../assets/images/cards/business.4fa3b853.jpg');
+            background-size:cover;
+            // background-size: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            width: 100%;
+            height: 100%;
+             -webkit-clip-path: polygon(0 0, 0 100%, 93% 100%, 100% 0);
+             clip-path: polygon(0 0, 0 100%, 95% 100%, 100% 0);
+        }
+
+        .text{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 10px;
+            h2{
+                font-size: 2rem;
+                margin: 10px 0px;
+            }
+            p{
+                margin: 10px 0px;
+                font-size: 1rem;
+            }
+            
+        }
+
+
+        .btn{
+            text-align: center;
+            width: 100px;
+            border-radius:5px;
+            background-color:rgb(0, 204, 188);
+            color: white;
+            padding: 10px;
+            margin-top: 10px;
+        }
+     }
+    }
+    
+
+    @media screen and (min-width: 650px) {
+        .container{
+            width: 60%;
+            min-height: 210px;
+        }
+        
+    }
+
+</style>
