e4ba2c897a95766092cf1392bf8bfac910ff6abf
Gianluca Benassi <benox1199@gmail.com>
2022-03-21 23:23:36 +0100

    statistics link + statistics index style

--- backend/resources/views/admin/statistics/index.blade.php
+++ backend/resources/views/admin/statistics/index.blade.php
@@ -2,11 +2,92 @@
 
 @section('content')
 
-<div class="container">
-    <canvas id="revenueChart" class="mb-5"></canvas>
-    <canvas id="orderNumberChart" class="mb-5"></canvas>
-    <canvas id="dishesRankChart" class="mb-5"></canvas>
-</div>
+<div class="container index-statistics">
+      <div class="row">
+
+          {{-- benvenuto/gestisci utente --}}
+          <div class="col-lg-9 statistic">
+              <div class="benvenuto-statistic mb-3">
+                <h1 class="text-center">Statistiche</h1>
+              </div>
+  
+              <div class="row no-gutters">
+                  <div class="col">
+                    <canvas id="revenueChart" class="mb-5"></canvas>
+                    <canvas id="orderNumberChart" class="mb-5"></canvas>
+                    <canvas id="dishesRankChart" class="mb-5"></canvas>
+                  </div>
+          </div>
+        </div>
+
+        {{-- aside lg --}}
+        <aside class="col-lg-3 px-0 aside">
+
+          {{-- card utente --}}
+          <h3 class="user-title">
+            Utente connesso
+          </h3>
+          <div id="user-info">
+            <ul>
+              {{-- nome utente --}}
+              <li>
+                <div>
+                  <i class="fa-solid fa-user-large"></i>
+                  <span>Nome utente</span>
+                </div>
+                <span class="info">
+                  {{Auth::user()->name}} {{Auth::user()->surname}}
+                </span>
+              </li>
+              {{-- email utente --}}
+              <li>
+                <div>
+                  <i class="fa-solid fa-envelope"></i>
+                  <span>Email</span>
+                </div>
+                <span class="info">
+                  {{Auth::user()->email}}
+                </span>
+              </li>
+              {{-- partita iva utente --}}
+              <li>
+                <div>
+                  <i class="fa-solid fa-briefcase"></i>
+                  <span>Partita iva</span>
+                </div>
+                <span class="info">
+                  {{Auth::user()->vat}}
+                </span>
+              </li>
+            </ul>
+          </div>
+
+          {{-- container bottoni --}}
+          @if($restaurant)
+          <div id="btn-container">
+            {{-- bottone ristorante --}}
+            <a href="{{ route('restaurants.index') }}" class="text-center restaurant-color">
+            <button class="btn">
+                <i class="fa-solid fa-house-chimney-user"></i> Ristorante
+            </button>
+            </a>
+            {{-- bottone menu --}}
+            <a href="{{route('dishes.index')}}" class="text-center menu-color">
+                <button class="btn">
+                    <i class="fas fa-utensils responsive-i" aria-hidden="true"></i> Menù
+                </button>
+            </a>
+            {{-- bottone ordini --}}
+            <a href="{{route('orders.index')}}" class="text-center orders-color">
+                <button class="btn">
+                    <i class="fa-solid fa-basket-shopping"></i> Ordini
+                </button>
+            </a>
+
+          </div>
+          @endif
+        </aside>
+      </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
@@ -157,8 +238,8 @@
         datasets: [
             {
                 label: 'Classifica piatti',
-                backgroundColor: '#D0EB99',
-                borderColor: '#D0EB99',
+                backgroundColor: '#007E8A',
+                borderColor: '#007E8A',
                 data: dishesRankData,
             },
         ]
