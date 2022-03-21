46dd7035993e7d0a6b1977c5ef254d5fa3a4eca7
Gianluca Benassi <benox1199@gmail.com>
2022-03-21 14:13:09 +0100

    restaurants pagination fix

--- backend/app/Http/Controllers/Api/RestaurantController.php
+++ backend/app/Http/Controllers/Api/RestaurantController.php
@@ -7,6 +7,7 @@ use App\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
 
 class RestaurantController extends Controller
@@ -19,11 +20,42 @@ class RestaurantController extends Controller
     public function filterCategory(Request $request){
         $data = $request->all();
 
-        $restaurants = Restaurant::select('address','city','country','description','email','image_cover','name','phone','post_code','restaurant_id as id','slug')
+        // $restaurants = Restaurant::select('address','city','country','description','email','image_cover','name','phone','post_code','restaurant_id as id','slug')
+        // ->join('category_restaurant','restaurants.id','=','category_restaurant.restaurant_id')
+        // ->whereIn('category_restaurant.category_id',$data['categories'])
+        // ->distinct()
+        // ->paginate(10);
+
+        $totalRestaurants = Restaurant::select('address','city','country','description','email','image_cover','name','phone','post_code','restaurant_id as id','slug')
         ->join('category_restaurant','restaurants.id','=','category_restaurant.restaurant_id')
         ->whereIn('category_restaurant.category_id',$data['categories'])
         ->distinct()
-        ->paginate(10);
+        ->get();
+
+        $total = count($totalRestaurants);
+        $per_page = 10;
+        $current_page = $data['page'];
+
+        $starting_point = ($current_page * $per_page) - $per_page;
+
+        $restaurants = $totalRestaurants->toArray();
+        $restaurants = array_slice($restaurants, $starting_point, $per_page, true);
+
+        $restaurants = new Paginator($restaurants, $total, $per_page, $current_page, [
+            'path' => $request->url(),
+            'query' => $request->query()
+        ]);
+
+        // $totalRestaurant = Restaurant::select('name')
+        // ->join('category_restaurant','restaurants.id','=','category_restaurant.restaurant_id')
+        // ->whereIn('category_restaurant.category_id',$data['categories'])
+        // ->distinct()->get();
+
+        // if($totalRestaurant % 10 == 0) {
+        //     $fixPages = true;
+        // } else {
+        //     $fixPages = false;
+        // }
 
         return response()->json($restaurants);
     }
