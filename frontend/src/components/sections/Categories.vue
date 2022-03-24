b547c9e8b4075b3193729c5985d8530b91bbc8d9
Daniele Di Eleuterio <daniele.dieleuterio@gmail.com>
2022-03-24 15:13:00 +0100

    media query smartphone

--- frontend/src/components/sections/Categories.vue
+++ frontend/src/components/sections/Categories.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="my_container">
     <div class="title">
         <h1>Cosa stai cercando?</h1>
     </div>
@@ -37,7 +37,7 @@ export default {
 
 <style lang = "scss" scoped>
 @import '../../assets/style/variables.scss';
-.container-fluid{
+.my_container{
     .title{
         h1{
             margin-top: -50px;
@@ -45,10 +45,13 @@ export default {
             background-color: $_cherry;
             color: white;
             padding: 0.4rem 1rem;
+            clip-path: polygon(0 10%, 97% 0, 100% 100%, 0% 100%);
+            /* text-transform: uppercase; */
         }
         color: $_blue;
         /* text-shadow: 2px 2px 4px black; */
         padding: 20px 0;
+        
     }
 
     p.under-title {
@@ -124,6 +127,10 @@ export default {
                 display: absolute;
                 position: absolute;
                 opacity: 0;
+                &:hover + label{
+                    background-color: $_cherry;
+                    border: 1.5px solid $_cherry;
+                }
                 
                 &:checked + label {
                     /* background-color: $_primary; */
@@ -144,6 +151,28 @@ export default {
                     transform: rotate(-360deg);
                     transition: transform .3s ease-in-out;
                 }
+            }
+        }
+    }
+}
+@media screen and (max-width: 329px) {
+    .my_container{
+        margin-top: 15px;
+        ul.edit-container{
+            margin: 30px 0 0 0;
+            padding: 0;
+            /* justify-content: flex-start; */
+
+            li{
+                margin: 0;
+                padding: 0;
+                label {
+                    margin: 1px;
+                    padding: 0.2rem 0;
+
+                    font-weight: 400;
+                    width: 7.3rem;
+                }                
             }
         }
     }
