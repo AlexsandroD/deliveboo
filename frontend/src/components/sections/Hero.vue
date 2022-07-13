a1479e0095c370389168557d4be8f0c756add460
AlexsandroD <adrdra88@gmail.com>
2022-07-13 19:19:29 +0200

    correzione

--- frontend/src/components/sections/Hero.vue
+++ frontend/src/components/sections/Hero.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hero">
     <video autoplay muted loop>
-      <source
+      <!-- <source
         src="../../assets/video/outputcompress-video-online.com.mp4"
         type="video/mp4"
-      />
-      <!-- <source src="../../assets/video/video.mp4" type="video/mp4" /> -->
+      /> -->
+      <source src="../../assets/video/video.mp4" type="video/mp4" />
       <source src="movie.ogg" type="video/ogg" />
       Your browser does not support the video tag.
     </video>
